e0beb73a024247c964591ff986b4ce9998fea95b
吴浠 <361125982@qq.com>
2016-02-01 20:00:17 +0800

    Initial Commit

---
+++ IOSAnimationSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSAnimationSample
+//
+//  Created by 吴浠 on 16/2/1.
+//  Copyright © 2016年 吴浠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSAnimationSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSAnimationSample
+//
+//  Created by 吴浠 on 16/2/1.
+//  Copyright © 2016年 吴浠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
